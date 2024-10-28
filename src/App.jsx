0fa8582c3da8def8be7cef8c5264ff3c7339b233
Juliia Prokopenko <atlasovaa13@gmail.com>
2024-10-28 16:37:38 +0100

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactForm from './components/ContactForm/ContactForm';
+
 import './App.css';
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <h1 className="title">Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </>
+  );
 }
-
-export default App;
